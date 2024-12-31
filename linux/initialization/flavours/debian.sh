c6073ad9d702fcfcd4eef38425204de38bed895a
Ivaylo Ivanov <ivaylolivanov95@gmail.com>
2024-12-31 11:33:51 +0200

    debian: Add pip to the package list
    Signed-off-by: Ivaylo Ivanov <ivaylolivanov95@gmail.com>

--- linux/initialization/flavours/debian.sh
+++ linux/initialization/flavours/debian.sh
@@ -68,7 +68,7 @@ function install-packages()
         htop libssl-dev libdbus-1-dev picom vulkan-tools libvulkan-dev rustc jq \
         qbittorrent pavucontrol thunderbird software-properties-common texlive  \
         imagemagick shellcheck curl ssh dvipng conky-all rsync xorg-dev gdb     \
-        || return 2;
+        python3-pip || return 2;
 
     install-non-native-packages || return 3;
 
